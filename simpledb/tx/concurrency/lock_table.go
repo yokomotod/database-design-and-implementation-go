63b2d2fbc64f0932d64bfead4bd147320c88d707
Takayuki Matsubara <t-matsubara@m3.com>
2024-07-27 13:04:42 +0900

    implement Java like `wait` function

--- simpledb/tx/concurrency/lock_table.go
+++ simpledb/tx/concurrency/lock_table.go
@@ -31,19 +31,15 @@ func (l *LockTable) SLock(blockID file.BlockID) error {
 	l.mux.Lock()
 	defer l.mux.Unlock()
 
-	timeoutChan := time.After(maxLockTime)
+	startTime := time.Now()
 	for {
-		select {
-		case <-timeoutChan:
+		if time.Since(startTime) > maxLockTime {
 			return ErrTimeout
-		default:
-			if !l.hasXLock(blockID) {
-				goto notlocked
-			}
+		} else if !l.hasXLock(blockID) {
+			break
 		}
-		l.cond.Wait()
+		l.waitWithTimeout(maxLockTime)
 	}
-notlocked:
 
 	l.locks[blockID]++
 	return nil
@@ -53,19 +49,15 @@ func (l *LockTable) XLock(blockID file.BlockID) error {
 	l.mux.Lock()
 	defer l.mux.Unlock()
 
-	timeoutChan := time.After(maxLockTime)
+	startTime := time.Now()
 	for {
-		select {
-		case <-timeoutChan:
+		if time.Since(startTime) > maxLockTime {
 			return ErrTimeout
-		default:
-			if !l.hasOtherSLocks(blockID) {
-				goto notlocked
-			}
+		} else if !l.hasOtherSLocks(blockID) {
+			break
 		}
-		l.cond.Wait()
+		l.waitWithTimeout(maxLockTime)
 	}
-notlocked:
 
 	l.locks[blockID] = -1
 	return nil
@@ -89,4 +81,19 @@ func (l *LockTable) hasXLock(blockID file.BlockID) bool {
 
 func (l *LockTable) hasOtherSLocks(blockID file.BlockID) bool {
 	return l.locks[blockID] > 1
+}
+
+// Java の `wait(MAX_TIME)` 相当を実現するために追加
+func (l *LockTable) waitWithTimeout(timeout time.Duration) {
+	timer := time.AfterFunc(timeout, func() {
+		l.mux.Lock()
+		defer l.mux.Unlock()
+		l.cond.Broadcast()
+	})
+	l.cond.Wait()
+
+	// NOTE: タイミング次第で `Stop()` 呼び出し前にすでに timer が動いてしまう可能性あり
+	// その場合 Broadcast が実行されてしまうが、その際に Wait があってもなくても
+	// 今の処理であればループでロック状態を確認しており特に問題はなさそうに見える
+	timer.Stop()
 }
