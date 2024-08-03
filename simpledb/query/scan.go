92c274506b636620dd3bd9047623b3fb35c1a880
Ken Sudo <ken-sudo2@m3.com>
2024-08-04 01:56:16 +0900

    move UpdateScan to scan.go

--- simpledb/query/scan.go
+++ simpledb/query/scan.go
@@ -11,3 +11,12 @@ type Scan interface {
 	HasField(fieldName string) bool
 	Close()
 }
+type UpdateScan interface {
+	SetVal(fieldName string, val *record.Constant) error
+	SetInt(fieldName string, val int32) error
+	SetString(fieldName string, val string) error
+	Insert() error
+	Delete() error
+	GetRID() (*record.RID, error)
+	MoveToRID(rid *record.RID) error
+}
