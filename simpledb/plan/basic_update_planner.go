ca070339d0cd1beb99a716bf651130381e126e4c
takumikid <takumi-kida@m3.com>
2024-08-15 00:07:47 +0900

    Ch 10.4, 10.5 Basic Query/Update planner

---
+++ simpledb/plan/basic_update_planner.go
@@ -0,0 +1,154 @@
+package plan
+
+import (
+	"fmt"
+	"simpledb/metadata"
+	"simpledb/parse"
+	"simpledb/query"
+	"simpledb/tx"
+)
+
+type BasicUpdatePlanner struct {
+	mdm *metadata.Manager
+}
+
+func NewBasicUpdatePlanner(mdm *metadata.Manager) *BasicUpdatePlanner {
+	return &BasicUpdatePlanner{mdm: mdm}
+}
+
+func (up *BasicUpdatePlanner) executeInsert(data *parse.InsertData, tx *tx.Transaction) (int, error) {
+	tableName := data.TableName
+	tablePlan, err := NewTablePlan(tx, tableName, up.mdm)
+	if err != nil {
+		return 0, err
+	}
+
+	scan, err := tablePlan.Open()
+	if err != nil {
+		return 0, err
+	}
+	defer scan.Close()
+
+	us, ok := scan.(query.UpdateScan)
+	if !ok {
+		return 0, fmt.Errorf("cannot insert: %v", data)
+	}
+	err = us.Insert()
+	if err != nil {
+		return 0, err
+	}
+	for i, field := range data.Fields {
+		value := data.Values[i]
+		err = us.SetVal(field, value)
+		if err != nil {
+			return 0, err
+		}
+	}
+	return 1, nil
+}
+
+func (up *BasicUpdatePlanner) executeDelete(data *parse.DeleteData, tx *tx.Transaction) (int, error) {
+	tableName := data.TableName
+	tablePlan, err := NewTablePlan(tx, tableName, up.mdm)
+	if err != nil {
+		return 0, err
+	}
+
+	selectPlan, err := NewSelectPlan(tablePlan, data.Pred)
+	if err != nil {
+		return 0, err
+	}
+
+	scan, err := selectPlan.Open()
+	if err != nil {
+		return 0, err
+	}
+	defer scan.Close()
+
+	us, ok := scan.(query.UpdateScan)
+	if !ok {
+		return 0, fmt.Errorf("cannot delete: %v", data)
+	}
+	count := 0
+	for {
+		hasNext, err := scan.Next()
+		if err != nil {
+			return 0, err
+		}
+		if !hasNext {
+			break
+		}
+		err = us.Delete()
+		if err != nil {
+			return 0, err
+		}
+		count++
+	}
+	return count, nil
+}
+
+func (up *BasicUpdatePlanner) ExecuteModify(data *parse.ModifyData, tx *tx.Transaction) (int, error) {
+	tableName := data.TableName
+	tablePlan, err := NewTablePlan(tx, tableName, up.mdm)
+	if err != nil {
+		return 0, err
+	}
+
+	selectPlan, err := NewSelectPlan(tablePlan, data.Pred)
+	if err != nil {
+		return 0, err
+	}
+
+	scan, err := selectPlan.Open()
+	if err != nil {
+		return 0, err
+	}
+	defer scan.Close()
+
+	us, ok := scan.(query.UpdateScan)
+	if !ok {
+		return 0, fmt.Errorf("cannot modify: %v", data)
+	}
+	count := 0
+	for {
+		hasNext, err := scan.Next()
+		if err != nil {
+			return 0, err
+		}
+		if !hasNext {
+			break
+		}
+		newValue, err := data.NewValue.Evaluate(scan)
+		if err != nil {
+			return 0, err
+		}
+		err = us.SetVal(data.TargetField, newValue)
+		if err != nil {
+			return 0, err
+		}
+		count++
+	}
+	return count, nil
+}
+
+func (up *BasicUpdatePlanner) ExecuteCreateTable(data *parse.CreateTableData, tx *tx.Transaction) (int, error) {
+	tableName := data.TableName
+	schema := data.NewSchema
+	err := up.mdm.CreateTable(tableName, schema, tx)
+	return 0, err
+}
+
+func (up *BasicUpdatePlanner) ExecuteCreateView(data *parse.CreateViewData, tx *tx.Transaction) (int, error) {
+	viewName := data.ViewName
+	viewDef := data.ViewDef()
+	err := up.mdm.CreateView(viewName, viewDef, tx)
+	return 0, err
+}
+
+func (up *BasicUpdatePlanner) ExecuteCreateIndex(data *parse.CreateIndexData, tx *tx.Transaction) (int, error) {
+	indexName := data.IndexName
+	tableName := data.TableName
+	fieldName := data.FieldName
+	err := up.mdm.CreateIndex(indexName, tableName, fieldName, tx)
+	return 0, err
+}
---
+++ simpledb/plan/planner.go
@@ -0,0 +1,96 @@
+package plan
+
+import (
+	"fmt"
+	"simpledb/parse"
+	"simpledb/tx"
+)
+
+type QueryPlanner interface {
+	CreatePlan(querydata *parse.QueryData, tx *tx.Transaction) (Plan, error)
+}
+
+type UpdatePlanner interface {
+	ExecuteInsert(insertdata *parse.InsertData, tx *tx.Transaction) (int, error)
+	ExecuteDelete(deletedata *parse.DeleteData, tx *tx.Transaction) (int, error)
+	ExecuteModify(modifydata *parse.ModifyData, tx *tx.Transaction) (int, error)
+	ExecuteCreateTable(createtabledata *parse.CreateTableData, tx *tx.Transaction) (int, error)
+	ExecuteCreateView(createviewdata *parse.CreateViewData, tx *tx.Transaction) (int, error)
+	ExecuteCreateIndex(createindexdata *parse.CreateIndexData, tx *tx.Transaction) (int, error)
+}
+
+type Planner struct {
+	queryPlanner  QueryPlanner
+	updatePlanner UpdatePlanner
+}
+
+func NewPlanner(queryPlanner QueryPlanner, updatePlanner UpdatePlanner) *Planner {
+	return &Planner{
+		queryPlanner:  queryPlanner,
+		updatePlanner: updatePlanner,
+	}
+}
+
+func (p *Planner) createQueryPlan(query string, tx *tx.Transaction) (Plan, error) {
+	parser, err := parse.NewParser(query)
+	if err != nil {
+		return nil, err
+	}
+
+	querydata, err := parser.Query()
+	if err != nil {
+		return nil, err
+	}
+
+	err = p.verifyQuery(querydata)
+	if err != nil {
+		return nil, err
+	}
+
+	return p.queryPlanner.CreatePlan(querydata, tx)
+
+}
+
+func (p *Planner) executeUpdate(cmd string, tx *tx.Transaction) (int, error) {
+	parser, err := parse.NewParser(cmd)
+	if err != nil {
+		return 0, err
+	}
+
+	updateCmd, err := parser.UpdateCmd()
+	if err != nil {
+		return 0, err
+	}
+
+	err = p.verifyUpdate(updateCmd)
+	if err != nil {
+		return 0, err
+	}
+
+	switch cmd := updateCmd.(type) {
+	case *parse.InsertData:
+		return p.updatePlanner.ExecuteInsert(cmd, tx)
+	case *parse.DeleteData:
+		return p.updatePlanner.ExecuteDelete(cmd, tx)
+	case *parse.ModifyData:
+		return p.updatePlanner.ExecuteModify(cmd, tx)
+	case *parse.CreateTableData:
+		return p.updatePlanner.ExecuteCreateTable(cmd, tx)
+	case *parse.CreateViewData:
+		return p.updatePlanner.ExecuteCreateView(cmd, tx)
+	case *parse.CreateIndexData:
+		return p.updatePlanner.ExecuteCreateIndex(cmd, tx)
+	default:
+		return 0, fmt.Errorf("unexpected update command: %v", cmd)
+	}
+}
+
+func (p *Planner) verifyQuery(queryData *parse.QueryData) error {
+	// TODO implement
+	return nil
+}
+
+func (p *Planner) verifyUpdate(updateCmd parse.UpdateCmd) error {
+	// TODO implement
+	return nil
+}
