1c0628dcb741583f81b052db37464a3cbc475b16
takumikid <takumi-kida@m3.com>
2024-08-15 16:57:26 +0900

    format

--- simpledb/plan/planner_test.go
+++ simpledb/plan/planner_test.go
@@ -16,26 +16,25 @@ func TestStudent(t *testing.T) {
 		t.Fatalf("failed to create tx: %v", err)
 	}
 
-	planner:= simpleDB.Planner()
+	planner := simpleDB.Planner()
 
 	_, err = planner.ExecuteUpdate("create table student (sid int, sname varchar(10), majorid int, gradyear int)", tx)
-	if err !=nil{
+	if err != nil {
 		t.Fatalf("failed to create table: %v", err)
 	}
 	t.Logf("created table student")
 
 	cnt, err := planner.ExecuteUpdate("insert into student(sid, sname, majorid) values (1, 'joe', 10, 2021)", tx)
-	if err !=nil{
+	if err != nil {
 		t.Fatalf("failed to insert into table: %v", err)
 	}
 	t.Logf("insert into student: %d", cnt)
 
 	cnt, err = planner.ExecuteUpdate("insert into student(sid, sname, majorid) values (2, 'amy', 20, 2020)", tx)
-	if err !=nil{
+	if err != nil {
 		t.Fatalf("failed to insert into table: %v", err)
 	}
 	t.Logf("insert into student: %d", cnt)
-
 
 	plan, err := planner.CreateQueryPlan("select sid, sname from student", tx)
 	if err != nil {
@@ -43,26 +42,26 @@ func TestStudent(t *testing.T) {
 	}
 
 	sc, err := plan.Open()
-	if err != nil{
+	if err != nil {
 		t.Fatalf("failed to open scan: %v", err)
 	}
 	defer sc.Close()
 
 	for {
 		next, err := sc.Next()
-		if err != nil{
+		if err != nil {
 			t.Fatalf("failed to get next: %v", err)
 		}
-		if !next{
+		if !next {
 			break
 		}
 
 		sid, err := sc.GetInt("sid")
-		if err !=nil{
+		if err != nil {
 			t.Fatalf("failed to get int: %v", err)
 		}
 		sname, err := sc.GetString("sname")
-		if err !=nil{
+		if err != nil {
 			t.Fatalf("failed to get string: %v", err)
 		}
 		// sid: 1, sname: joe
