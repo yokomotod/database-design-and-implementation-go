f46c798f68c1f4a44917e49dd14dbb5b2791a046
Ryosuke Takashima <ryosuke-takashima@m3.com>
2024-08-20 00:23:18 +0900

    add transaction to driver

--- simpledb/driver/driver_test.go
+++ simpledb/driver/driver_test.go
@@ -14,27 +14,52 @@ func TestDriver(t *testing.T) {
 		t.Fatalf("failed to open db: %v", err)
 	}
 
+	tx2, err := db.Begin()
+	if err != nil {
+		t.Fatalf("failed to begin transaction: %v", err)
+	}
 	createTable(t, db, "create table player (player_id int, name varchar(10), birth_year int, country varchar(10), point int)")
+	err = tx2.Commit()
+	if err != nil {
+		t.Fatalf("failed to commit: %v", err)
+	}
 
+	tx5, _ := db.Begin()
 	insert(t, db, "insert into player (player_id, name, birth_year, country, point) values (1, 'Nobak', 1987, 'Serbia', 11055)")
 	insert(t, db, "insert into player (player_id, name, birth_year, country, point) values (2, 'Carlos', 2003, 'Spain', 8855)")
 	insert(t, db, "insert into player (player_id, name, birth_year, country, point) values (3, 'Daniil', 1996, 'Russia', 7555)")
 	insert(t, db, "insert into player (player_id, name, birth_year, country, point) values (4, 'Jannik', 2001, 'Italy', 6490)")
+	err = tx5.Commit()
+	if err != nil {
+		t.Fatalf("failed to commit: %v", err)
+	}
 
+	tx6, _ := db.Begin()
 	queryPlayer(t, db)
-
 	update(t, db, "update player set point = 8360 where player_id = 1")
 	update(t, db, "update player set point = 8130 where player_id = 2")
 	update(t, db, "update player set point = 6445 where player_id = 3")
 	update(t, db, "update player set point = 9890 where player_id = 4")
+	err = tx6.Commit()
+	if err != nil {
+		t.Fatalf("failed to commit: %v", err)
+	}
 
+	tx7, _ := db.Begin()
 	queryPlayer(t, db)
-
 	update(t, db, "update player set point = 0")
+	err = tx7.Rollback()
+	if err != nil {
+		t.Fatalf("failed to rollback: %v", err)
+	}
 
+	tx8, _ := db.Begin()
 	queryPlayer(t, db)
-
 	delete(t, db, "delete from player")
+	err = tx8.Commit()
+	if err != nil {
+		t.Fatalf("failed to commit: %v", err)
+	}
 
 	defer db.Close()
 }
