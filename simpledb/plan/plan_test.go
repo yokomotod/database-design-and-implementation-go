40579b1e6beea5932a1bbf70ab7126d498f2d6c7
natsuokawai <kawai.natsuo@gmail.com>
2024-09-01 13:52:36 +0900

    add SortScan and SortPlan

--- simpledb/plan/plan_test.go
+++ simpledb/plan/plan_test.go
@@ -6,6 +6,7 @@ import (
 	"simpledb/plan"
 	"simpledb/query"
 	"simpledb/server"
+	"simpledb/testlib"
 	"testing"
 )
 
@@ -14,7 +15,7 @@ func TestSingleTablePlan(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to create simpledb: %v", err)
 	}
-	err = insertTestData(simpleDB)
+	err = testlib.InsertTestData(t, simpleDB)
 	if err != nil {
 		t.Fatalf("failed to setup test data: %v", err)
 	}
@@ -77,7 +78,7 @@ func TestMultipleTablePlan(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to create simpledb: %v", err)
 	}
-	err = insertTestData(simpleDB)
+	err = testlib.InsertTestData(t, simpleDB)
 	if err != nil {
 		t.Fatalf("failed to setup test data: %v", err)
 	}
@@ -132,86 +133,4 @@ func printStats(n int, p plan.Plan) {
 	fmt.Printf("\tR(%s): %d\n", planName, p.RecordsOutput())
 	fmt.Printf("\tB(%s): %d\n", planName, p.BlocksAccessed())
 	fmt.Println()
-}
-
-func insertTestData(simpledb *server.SimpleDB) error {
-	tx, err := simpledb.NewTx()
-	if err != nil {
-		return err
-	}
-
-	//create table
-	planner := simpledb.Planner()
-
-	_, err = planner.ExecuteUpdate("create table student(sid int, sname varchar(10), majorid int, gradyear int) ", tx)
-	if err != nil {
-		return err
-	}
-	_, err = planner.ExecuteUpdate("create table dept(did int, dname varchar(8))", tx)
-	if err != nil {
-		return err
-	}
-
-	depts := []struct {
-		did   int
-		dname string
-	}{
-		{10, "compsci"},
-		{20, "math"},
-		{30, "drama"},
-	}
-
-	for _, d := range depts {
-		query := fmt.Sprintf("insert into dept(did, dname) values(%d, '%s')", d.did, d.dname)
-		_, err = planner.ExecuteUpdate(query, tx)
-		if err != nil {
-			return err
-		}
-	}
-
-	students := []struct {
-		sid      int
-		sname    string
-		majorid  int
-		gradyear int
-	}{
-		{1, "joe", 10, 2021},
-		{2, "amy", 20, 2020},
-		{3, "max", 10, 2022},
-		{4, "sue", 20, 2022},
-		{5, "bob", 30, 2020},
-		{6, "kim", 20, 2020},
-		{7, "art", 30, 2021},
-		{8, "pat", 10, 2022},
-		{9, "lee", 10, 2021},
-		{10, "dan", 20, 2020},
-	}
-
-	for _, s := range students {
-		query := fmt.Sprintf("insert into student(sid, sname, majorid, gradyear) values(%d, '%s', %d, %d)", s.sid, s.sname, s.majorid, s.gradyear)
-		_, err = planner.ExecuteUpdate(query, tx)
-		if err != nil {
-			return err
-		}
-	}
-
-	// 強制的に統計情報を更新 (Metadata Managerに機能追加を検討)
-	mdm := simpledb.MetadataManager()
-	studentLayout, err := mdm.GetLayout("student", tx)
-	if err != nil {
-		return err
-	}
-	for i := 0; i <= 100; i++ {
-		_, err = mdm.GetStatInfo("student", studentLayout, tx)
-		if err != nil {
-			return err
-		}
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
